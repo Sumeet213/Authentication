7c5d2d538c295d133efe247e4776c8adf2ad4023
Sumeet213 <75944652+Sumeet213@users.noreply.github.com>
2022-07-23 17:23:46 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const homeController = require("../controllers/home_controller");
 const app = express();
 console.log("router loaded");
 
-router.get("/", homeController.home);
+router.get("/",checkAuthenticated, homeController.home);
 router.use("/users", require("./users"));
 
 app.use(passport.initialize());
